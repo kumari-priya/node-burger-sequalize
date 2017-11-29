fdecee91af8944ecb27b80f21f56dca4731ca723
Kumari Priya <priya@goel.pw>
2017-11-28 21:02:18 -0800

    reload page after add/update

--- controllers/burgers-routes.js
+++ controllers/burgers-routes.js
@@ -2,51 +2,49 @@ var db = require("../models");
 
 module.exports = function(app) {
 
-// Create all our routes and set up logic within those routes where required.
-app.get("/", function(req, res) {
-  db.Burger.findAll({include: [db.Person]}).then(function(result) {
-    // We have access to the todos as an argument inside of the callback function
-  //res.json(result);
-  console.log("result");
-  console.log(result);
-    return res.render('index', {result})
+  // Create all our routes and set up logic within those routes where required.
+  app.get("/", function(req, res) {
+    db.Burger.findAll({
+      include: [db.Person]
+    }).then(function(result) {
+      return res.render('index', {
+        result
+      })
+    });
   });
-});
 
-app.post("/api/burgers", function(req, res) {
-  db.Burger.create({
-    burger_name: req.body.burger_name,
-    devoured: req.body.devoured
-  }).then(function(result) {
-    // We have access to the new todo as an argument inside of the callback function
-    //res.json({ id: result.insertId });
-    res.json({ id: result.insertId });
-
-  })
-  .catch(function(err) {
-    // Whenever a validation or flag fails, an error is thrown
-    // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-    res.json(err);
+  app.post("/api/burgers", function(req, res) {
+    db.Burger.create({
+        burger_name: req.body.burger_name,
+        devoured: req.body.devoured
+      }).then(function(result) {
+        res.json({
+          id: result.id
+        });
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
   });
-});
 
-app.put("/api/burgers/:id", function(req, res) {
-  db.Burger.update(
-  {  devoured: req.body.devoured,
-    name: req.body.name
-  },
- {
-    where: {
-      id: req.body.id
-    }
-  }).then(function(result) {
-    res.json(result);
-  })
-  .catch(function(err) {
-    // Whenever a validation or flag fails, an error is thrown
-    // We can "catch" the error to prevent it from being "thrown", which could crash our node app
-    res.json(err);
+  app.put("/api/burgers/:id", function(req, res) {
+    db.Person.create({
+      name: req.body.name
+    }).then(function(result) {
+      var personID = result.id;
+      return db.Burger.update({
+        devoured: req.body.devoured,
+        PersonId: personID
+      }, {
+        where: {
+          id: req.body.id
+        }
+      })
+    }).then(function(result) {
+      res.json(result);
+    }).catch(function(err) {
+      res.json(err);
+    })
   });
-});
 
 };
